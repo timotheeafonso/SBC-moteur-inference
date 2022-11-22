0cc61bb41976e257ed78e9a74820d89e72266053
timothee afonso <tim.afonso22@gmail.com>
2022-11-22 09:13:10 +0100

    constructeur

--- src/Fait.java
+++ src/Fait.java
@@ -3,17 +3,27 @@ public class Fait {
 	
 	String element;
 	boolean etat;
-	
+	Integer val;
+
 	public Fait(String element, boolean etat) {
 		this.element = element;
 		this.etat = etat;
 	}
 
+	public Fait(String element, int val) {
+		this.element = element;
+		this.val = val;
+	}
+
 	@Override
 	public String toString() {
 		String pr= "";
-		if(!etat) {
-			pr+="non ";
+		if(this.val == null){
+			if(!etat) {
+				pr+="non ";
+			}
+		}else{
+			pr+="="+val;
 		}
 		pr += element;
 		return pr;
